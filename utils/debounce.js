ad6106bc2b06084a6f637dfa301ede3107062149
764Beef <beefrok1312@gmail.com>
2026-03-02 20:10:17 +0200

    Create debounce.js

---
+++ utils/debounce.js
@@ -0,0 +1,7 @@
+module.exports = function debounce(func, delay) {
+    let timeout;
+    return (...args) => {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, args), delay);
+    };
+};
